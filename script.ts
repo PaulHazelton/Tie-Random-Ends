d85af929d745414e6a27eb61d4988eb7da7ade9e
Paul Hazelton <hazeltonspaul@gmail.com>
2026-05-23 10:54:35 -0400

    Basic start

--- script.ts
+++ script.ts
@@ -1,2 +1,21 @@
-const name = Deno.args[0] ?? "World";
-console.log(`Hello ${name}!!!`);
+const initialNumStrings = getInput();
+
+console.log(`Initial strings: ${initialNumStrings}`);
+
+
+function getInput(): number {
+	const input = Deno.args[0] ?? "0";
+
+	try {
+		const result = Number.parseInt(input);
+
+		if (Number.isNaN(result)) {
+			console.error(`Bad input "${input}"`);	
+			return 0;
+		}
+		return result;
+	} catch {
+		console.error(`Bad input ${input}`);
+		return 0;
+	}
+}
